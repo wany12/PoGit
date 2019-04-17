48fe9d1e007b8d7882f9fe28f720f904786c51c1
Wan <wan11@hemera5.cluster>
2019-04-17 15:39:59 +0200

    add temporal evolution for a gaussian beam created from antenna

--- pogit/laser.py
+++ pogit/laser.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.constants import c
 
 from .codelets.laser import LaserProfile
-from .codelets.fieldBackground import LaserAntenna
+from .codelets.fieldBackground import LaserAntenna, distance_3d, laser_profile_2d, laser_profile_3d
 
 
 class Laser:
@@ -32,8 +32,8 @@ class Laser:
             Laser normalized amplitude
 
         ctau : float (in meters)
-            Laser duration as a longitudinal size (two RMS of the
-            power envelope)
+            Laser duration as two RMS of the intensity profile
+            ctau * 1.17741 = FWHM of the intensity profile
 
         waist : float (in meters)
             Laser waist (two RMS of the intensity profile)
@@ -91,7 +91,7 @@ class Laser:
         params['CEP'] = CEP
 
         if method=='native':
-            params['tau'] = ctau / c / 2.35482
+            params['tau'] = ctau / c / 2
             params['injection_duration'] = 2 * cdelay / c / params['tau']
             params['pol'] = { 'x':'LINEAR_X', 'z':'LINEAR_Z',
                               'circ':'CIRCULAR' }[pol]
@@ -104,7 +104,13 @@ class Laser:
             params['pol'] = { 'x':'1', 'z':'2', 'circ':'3' }[pol]
             params['ix_cntr'] = center_ij[0]
             params['iz_cntr'] = center_ij[1]
-
+            if dim=='3d':
+               params['distance_3d'] = Template(distance_3d).render(**params)
+               params['laser_profile'] = Template(laser_profile_3d).render(**params)
+            elif dim=='2d':
+               params['distance_3d'] = " "
+               params['laser_profile'] = Template(laser_profile_2d).render(**params)
+            
         # Converting float and integer arguments to strings
         for arg in params.keys():
             if type(params[arg]) == float:
